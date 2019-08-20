9b127fb0516433fac8338fb4671edeaa15a60154
limw <164918814@qq.com>
2019-08-20 19:25:59 +0800

    First time commit!

---
+++ R/BMIQ.R
@@ -0,0 +1,149 @@
+# Internal functions -----------------------------------------------------------
+
+BMIQ <- function(beta.v, design.v, nL = 3, doH = TRUE, nfit = 50000, th1.v = c(0.2,0.75),
+                 th2.v = NULL, niter = 5, tol = 0.001){
+
+  type1.idx <- which(design.v == 'I')
+  type2.idx <- which(design.v == 'II')
+  beta1.v <- beta.v[type1.idx]
+  beta2.v <- beta.v[type2.idx]
+
+  ### estimate initial weight matrix from type1 distribution
+  w0.m <- matrix(0, nrow = length(beta1.v), ncol = nL)
+  w0.m[which(beta1.v < th1.v[1]), 1] <- 1
+  w0.m[intersect(which(beta1.v >= th1.v[1]), which(beta1.v <= th1.v[2])), 2] <- 1
+  w0.m[which(beta1.v > th1.v[2]), 3] <- 1
+
+  ### fit type1 probe
+  set.seed(1)
+  rand.idx <- sample(1:length(beta1.v), min(c(nfit, length(beta1.v))), replace = FALSE)
+  em1.o <- blc2(Y = matrix(beta1.v[rand.idx], ncol=1),
+                w = w0.m[rand.idx, ], maxiter = niter, tol = tol)
+  subsetclass1.v <- apply(em1.o$w, 1, which.max)
+  subsetth1.v <- c(mean(max(beta1.v[rand.idx[subsetclass1.v == 1]]),
+                        min(beta1.v[rand.idx[subsetclass1.v == 2]])),
+                   mean(max(beta1.v[rand.idx[subsetclass1.v==2]]),
+                        min(beta1.v[rand.idx[subsetclass1.v==3]])))
+  class1.v <- rep(2, length(beta1.v))
+  class1.v[which(beta1.v < subsetth1.v[1])] <- 1
+  class1.v[which(beta1.v > subsetth1.v[2])] <- 3
+  nth1.v <- subsetth1.v
+
+  ### Estimate Modes
+  if (sum(class1.v == 1) == 1){ mod1U <- beta1.v[class1.v == 1] }
+  if (sum(class1.v == 3) == 1){ mod1M <- beta1.v[class1.v == 3] }
+  if (sum(class1.v == 1) > 1){
+    d1U.o <- density(beta1.v[class1.v == 1])
+    mod1U <- d1U.o$x[which.max(d1U.o$y)]
+  }
+  if (sum(class1.v == 3) > 1){
+    d1M.o <- density(beta1.v[class1.v == 3])
+    mod1M <- d1M.o$x[which.max(d1M.o$y)]
+  }
+
+  d2U.o <- density(beta2.v[which(beta2.v < 0.4)])
+  d2M.o <- density(beta2.v[which(beta2.v > 0.6)])
+  mod2U <- d2U.o$x[which.max(d2U.o$y)]
+  mod2M <- d2M.o$x[which.max(d2M.o$y)]
+
+  ### now deal with type2 fit
+  th2.v <- vector()
+  th2.v[1] <- nth1.v[1] + (mod2U - mod1U)
+  th2.v[2] <- nth1.v[2] + (mod2M - mod1M)
+
+  ### estimate initial weight matrix
+  w0.m <- matrix(0, nrow = length(beta2.v), ncol = nL)
+  w0.m[which(beta2.v <= th2.v[1]), 1] <- 1
+  w0.m[intersect(which(beta2.v > th2.v[1]), which(beta2.v <= th2.v[2])), 2] <- 1
+  w0.m[which(beta2.v > th2.v[2]), 3] <- 1
+
+  rand.idx <- sample(1:length(beta2.v), min(c(nfit, length(beta2.v))), replace = FALSE)
+  em2.o <- blc2(Y = matrix(beta2.v[rand.idx], ncol=1),
+                w = w0.m[rand.idx, ], maxiter = niter, tol = tol)
+
+  ### for type II probes assign to state (unmethylated, hemi or full methylation)
+  subsetclass2.v <- apply(em2.o$w, 1, which.max)
+
+  if (sum(subsetclass2.v == 2) > 0){
+    subsetth2.v <- c(mean(max(beta2.v[rand.idx[subsetclass2.v == 1]]),
+                          min(beta2.v[rand.idx[subsetclass2.v == 2]])),
+                    mean(max(beta2.v[rand.idx[subsetclass2.v == 2]]),
+                         min(beta2.v[rand.idx[subsetclass2.v == 3]])))
+  }
+  if (sum(subsetclass2.v == 2) == 0){
+    subsetth2.v <- c(1 / 2 * max(beta2.v[rand.idx[subsetclass2.v == 1]]) +
+                       1 / 2 * mean(beta2.v[rand.idx[subsetclass2.v == 3]]),
+                     1 / 3 * max(beta2.v[rand.idx[subsetclass2.v == 1]]) +
+                       2 / 3 * mean(beta2.v[rand.idx[subsetclass2.v == 3]]))
+  }
+
+  class2.v <- rep(2, length(beta2.v))
+  class2.v[which(beta2.v <= subsetth2.v[1])] <- 1
+  class2.v[which(beta2.v >= subsetth2.v[2])] <- 3
+
+  classAV1.v <- vector()
+  classAV2.v <- vector()
+  for(l in 1:nL){
+    classAV1.v[l] <- em1.o$mu[l, 1]
+    classAV2.v[l] <- em2.o$mu[l, 1]
+  }
+
+  ### start normalising input probes
+  nbeta2.v <- beta2.v
+  ### select U probes
+  lt <- 1
+  selU.idx <- which(class2.v == lt)
+  selUR.idx <- selU.idx[which(beta2.v[selU.idx] > classAV2.v[lt])]
+  selUL.idx <- selU.idx[which(beta2.v[selU.idx] < classAV2.v[lt])]
+  ### find prob according to typeII distribution
+  p.v <- pbeta(beta2.v[selUR.idx], em2.o$a[lt, 1],
+               em2.o$b[lt, 1], lower.tail = FALSE)
+  ### find corresponding quantile in type I distribution
+  q.v <- qbeta(p.v, em1.o$a[lt, 1],
+               em1.o$b[lt, 1], lower.tail = FALSE)
+  nbeta2.v[selUR.idx] <- q.v
+  p.v <- pbeta(beta2.v[selUL.idx], em2.o$a[lt, 1],
+               em2.o$b[lt, 1], lower.tail=TRUE)
+  ### find corresponding quantile in type I distribution
+  q.v <- qbeta(p.v, em1.o$a[lt, 1],
+               em1.o$b[lt, 1], lower.tail=TRUE)
+  nbeta2.v[selUL.idx] <- q.v
+
+  ### select M probes
+  lt <- 3
+  selM.idx <- which(class2.v == lt)
+  selMR.idx <- selM.idx[which(beta2.v[selM.idx] > classAV2.v[lt])]
+  selML.idx <- selM.idx[which(beta2.v[selM.idx] < classAV2.v[lt])]
+  ### find prob according to typeII distribution
+  p.v <- pbeta(beta2.v[selMR.idx], em2.o$a[lt, 1],
+               em2.o$b[lt, 1], lower.tail = FALSE)
+  ### find corresponding quantile in type I distribution
+  q.v <- qbeta(p.v, em1.o$a[lt, 1],
+               em1.o$b[lt, 1], lower.tail = FALSE)
+  nbeta2.v[selMR.idx] <- q.v
+
+  if(doH){
+    lt <- 2
+    selH.idx <- c(which(class2.v == lt), selML.idx)
+    minH <- min(beta2.v[selH.idx])
+    maxH <- max(beta2.v[selH.idx])
+    deltaH <- maxH - minH
+    deltaUH <- -max(beta2.v[selU.idx]) + min(beta2.v[selH.idx])
+    deltaHM <- -max(beta2.v[selH.idx]) + min(beta2.v[selMR.idx])
+
+    nmaxH <- min(nbeta2.v[selMR.idx]) - deltaHM
+    ## new minimum of H probes should be
+    nminH <- max(nbeta2.v[selU.idx]) + deltaUH
+    ndeltaH <- nmaxH - nminH
+
+    ### perform conformal transformation (shift+dilation)
+    hf <- ndeltaH / deltaH
+    nbeta2.v[selH.idx] <- nminH + hf * (beta2.v[selH.idx] - minH)
+  }
+
+  pnbeta.v <- beta.v
+  pnbeta.v[type1.idx] <- beta1.v
+  pnbeta.v[type2.idx] <- nbeta2.v
+
+  return(pnbeta.v)
+}
---
+++ R/qmqn_normalize.R
@@ -0,0 +1,91 @@
+#' Add together two numbers.
+#'
+#' @param m The matrix of methylation signal intensities.
+#' @param um The matrix of umethylation signal intensities.
+#' @param probe the probe id for m and um.
+#' @param type The type of methylation array which can be '450k'(default) or '850k'
+#' @param ref The reference used to normalize data
+#' @return A data frame contains normalized m and um, p value, and DNA methylation level
+qmqn_normalize <- function(m, um, probe, type = '450k', ref = list(t1.green.ref.mean=c(361.0165,9646.7525),
+                                                                    t1.green.ref.sd=c(278.9551,5109.423),
+                                                                    t1.red.ref.mean=c(696.0015,12036.3621),
+                                                                    t1.red.ref.sd=c(519.3141,7002.863))) {
+  if (type == '450k') {
+    t1.red <- row.names(annon_450k)[which(annon_450k$color == 'Red' & annon_450k$probe_type == 'I')]
+    t1.green <- row.names(annon_450k)[which(annon_450k$color == 'Grn' & annon_450k$probe_type == 'I')]
+    t2 <- row.names(annon_450k)[which(annon_450k$probe_type == 'II')]
+    type <- annon_450k[probe, 'probe_type']
+  } else if (type == '850k') {
+    t1.red <- row.names(annon_850k)[which(annon_850k$color == 'Red' & annon_850k$probe_type == 'I')]
+    t1.green <- row.names(annon_850k)[which(annon_850k$color == 'Grn' & annon_850k$probe_type == 'I')]
+    t2 <- row.names(annon_850k)[which(annon_850k$probe_type == 'II')]
+    type <- annon_850k[probe, 'probe_type']
+  }
+
+  set.seed(1)
+  print("Fitting Gaussian mixture model for probe of type 1 red----------")
+  t1.red.index <- match(t1.red, probe)
+  t1.red.index <- t1.red.index[which(!is.na(t1.red.index))]
+  t1.red.signal <- c(m[t1.red.index], um[t1.red.index])
+  t1.red.model <- Mclust(t1.red.signal, G=2)
+  t1.red.mean <- t1.red.model$parameters$mean
+  t1.red.sd <- sqrt(t1.red.model$parameters$variance$sigmasq)
+
+  print("Normalizing probe of type 1 red---------------------------------")
+  t1.red.signal[which(t1.red.model$classification == 1)] <- qnorm(pnorm(t1.red.signal[which(t1.red.model$classification == 1)],
+                                                                       t1.red.mean[1], t1.red.sd[1]), ref$t1.red.ref.mean[1], ref$t1.red.ref.sd[1])
+  t1.red.signal[which(t1.red.model$classification == 2)] <- qnorm(pnorm(t1.red.signal[which(t1.red.model$classification == 2)],
+                                                                       t1.red.mean[2], t1.red.sd[2]), ref$t1.red.ref.mean[2], ref$t1.red.ref.sd[2])
+  m[t1.red.index] <- t1.red.signal[1:(length(t1.red.signal)/2)]
+  um[t1.red.index] <- t1.red.signal[(length(t1.red.signal)/2+1):length(t1.red.signal)]
+
+  print("Fitting Gaussian mixture model for probe of type 1 green--------")
+  t1.green.index <- match(t1.green,probe)
+  t1.green.index <- t1.green.index[which(!is.na(t1.green.index))]
+  t1.green.signal <- c(m[t1.green.index],um[t1.green.index])
+  t1.green.model <- Mclust(t1.green.signal, G=2)
+  t1.green.mean <- t1.green.model$parameters$mean
+  t1.green.sd <- sqrt(t1.green.model$parameters$variance$sigmasq)
+
+  print("Normalizing probe of type 1 green-------------------------------")
+  t1.green.signal[which(t1.green.model$classification == 1)] <- qnorm(pnorm(t1.green.signal[which(t1.green.model$classification == 1)],
+                                                                           t1.green.mean[1], t1.green.sd[1]), ref$t1.green.ref.mean[1], ref$t1.green.ref.sd[1])
+  t1.green.signal[which(t1.green.model$classification == 2)] <- qnorm(pnorm(t1.green.signal[which(t1.green.model$classification == 2)],
+                                                                           t1.green.mean[2], t1.green.sd[2]), ref$t1.green.ref.mean[2], ref$t1.green.ref.sd[2])
+  m[t1.green.index] <- t1.green.signal[1:(length(t1.green.signal)/2)]
+  um[t1.green.index] <- t1.green.signal[(length(t1.green.signal)/2+1):length(t1.green.signal)]
+
+
+  print("Detecting p value-----------------------------------------------")
+  t2.index <- match(t2, probe)
+  t2.index <- t2.index[which(!is.na(t2.index))]
+  pIR <- apply(cbind(1 - pnorm(m[t1.red.index], t1.red.mean[1], t1.red.sd[1]),
+                     1 - pnorm(um[t1.red.index], t1.red.mean[1], t1.red.sd[1])),
+               1, min)
+  pIG <- apply(cbind(1 - pnorm(m[t1.green.index], t1.green.mean[1], t1.green.sd[1]),
+                     1 - pnorm(um[t1.green.index], t1.green.mean[1], t1.green.sd[1])),
+               1, min)
+  pII <- apply(cbind(1 - pnorm(um[t2.index], t1.green.mean[1], t1.green.sd[1]),
+                     1 - pnorm(m[t2.index], t1.red.mean[1], t1.red.sd[1])),
+               1, min)
+  p <- cbind(c(pIR, pIG , pII),
+          c(probe[t1.red.index], probe[t1.green.index], probe[t2.index]))
+  row.names(p) <- c(probe[t1.red.index], probe[t1.green.index], probe[t2.index])
+  m[which(m <= 0)] <- min(m[which(m > 0)])
+  um[which(um <= 0)] <- min(um[which(um > 0)])
+  normalized.signal <- data.frame(cbind(round(as.numeric(m), 0),
+                                       round(as.numeric(um), 0),
+                                       as.numeric(p[probe, 1])))
+  row.names(normalized.signal) <- probe
+  names(normalized.signal) <- c("m", "um", "p")
+
+  print("Normalizing between probe type----------------------------------")
+  normalized.signal[which(normalized.signal$m == Inf), "m"] <- max(normalized.signal$m[normalized.signal$m != Inf])
+  normalized.signal[which(normalized.signal$um == Inf), "um"] <- max(normalized.signal$um[normalized.signal$um != Inf])
+  beta <- normalized.signal$m / (normalized.signal$m + normalized.signal$um)
+  beta.adjust <- BMIQ(beta, type)
+  beta.adjust <- beta.adjust
+  normalized.signal$beta <- beta.adjust
+  return(normalized.signal)
+  print("Finished--------------------------------------------------------")
+}
