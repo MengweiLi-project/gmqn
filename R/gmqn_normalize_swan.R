345fa651e5031ae32586df359c37e895ff8ee0cc
limw <164918814@qq.com>
2019-11-04 17:56:52 +0800

    add swan

---
+++ R/gmqn_normalize_swan.R
@@ -0,0 +1,102 @@
+#' Gaussian Mixture Quantile Normalization (GMQN)
+#'
+#' The implementation of Gaussian Mixture Quantile Normalization (GMQN)
+#' @param m The vector of methylation signal intensities.
+#' @param um The vector of umethylation signal intensities.
+#' @param probe the probe id for m and um.
+#' @param type The type of methylation array which can be '450k'(default) or '850k'
+#' @param ref The reference used to normalize data. By default, it uses the reference set up from GSE105018 which is also used in EWAS Datahub.
+#' @return A data frame contains normalized m and um, p value, and DNA methylation level
+gmqn_normalize_swan <- function(m, um, probe, type = '450k', ref = 'default') {
+
+  if (ref == 'default') {
+    ref = list(t1.green.ref.mean=c(361.0165,9646.7525),
+               t1.green.ref.sd=c(278.9551,5109.423),
+               t1.red.ref.mean=c(696.0015,12036.3621),
+               t1.red.ref.sd=c(519.3141,7002.863))
+  }
+
+  if (type == '450k') {
+    t1.red <- row.names(annon_450k)[which(annon_450k$color == 'Red' & annon_450k$probe_type == 'I')]
+    t1.green <- row.names(annon_450k)[which(annon_450k$color == 'Grn' & annon_450k$probe_type == 'I')]
+    t2 <- row.names(annon_450k)[which(annon_450k$probe_type == 'II')]
+    type <- annon_450k[probe, 'probe_type']
+  } else if (type == '850k') {
+    t1.red <- row.names(annon_850k)[which(annon_850k$color == 'Red' & annon_850k$probe_type == 'I')]
+    t1.green <- row.names(annon_850k)[which(annon_850k$color == 'Grn' & annon_850k$probe_type == 'I')]
+    t2 <- row.names(annon_850k)[which(annon_850k$probe_type == 'II')]
+    type <- annon_850k[probe, 'probe_type']
+  }
+
+  set.seed(1)
+  print("Fitting Gaussian mixture model for probe of type 1 red----------")
+  t1.red.index <- match(t1.red, probe)
+  t1.red.index <- t1.red.index[which(!is.na(t1.red.index))]
+  t1.red.signal <- c(m[t1.red.index], um[t1.red.index])
+  t1.red.model <- Mclust(t1.red.signal, G=2, verbose = F)
+  t1.red.mean <- t1.red.model$parameters$mean
+  t1.red.sd <- sqrt(t1.red.model$parameters$variance$sigmasq)
+
+  pIR <- apply(cbind(1 - pnorm(m[t1.red.index], t1.red.mean[1], t1.red.sd[1]),
+                     1 - pnorm(um[t1.red.index], t1.red.mean[1], t1.red.sd[1])),
+               1, min)
+
+  print("Normalizing probe of type 1 red---------------------------------")
+  temp = t1.red.signal
+  t1.red.signal[which(t1.red.model$classification == 1)] <- qnorm(pnorm(t1.red.signal[which(t1.red.model$classification == 1)], t1.red.mean[1], t1.red.sd[1]), ref$t1.red.ref.mean[1], ref$t1.red.ref.sd[1])
+  t1.red.signal[which(t1.red.model$classification == 2)] <- qnorm(pnorm(t1.red.signal[which(t1.red.model$classification == 2)], t1.red.mean[2], t1.red.sd[2]), ref$t1.red.ref.mean[2], ref$t1.red.ref.sd[2])
+  t1.red.signal[which(t1.red.signal == Inf)] = temp[which(t1.red.signal == Inf)]
+
+  m[t1.red.index] <- t1.red.signal[1:(length(t1.red.signal)/2)]
+  um[t1.red.index] <- t1.red.signal[(length(t1.red.signal)/2+1):length(t1.red.signal)]
+
+  print("Fitting Gaussian mixture model for probe of type 1 green--------")
+  t1.green.index <- match(t1.green,probe)
+  t1.green.index <- t1.green.index[which(!is.na(t1.green.index))]
+  t1.green.signal <- c(m[t1.green.index],um[t1.green.index])
+  t1.green.model <- Mclust(t1.green.signal, G=2, verbose = F)
+  t1.green.mean <- t1.green.model$parameters$mean
+  t1.green.sd <- sqrt(t1.green.model$parameters$variance$sigmasq)
+
+  pIG <- apply(cbind(1 - pnorm(m[t1.green.index], t1.green.mean[1], t1.green.sd[1]),
+                     1 - pnorm(um[t1.green.index], t1.green.mean[1], t1.green.sd[1])),
+               1, min)
+
+  print("Normalizing probe of type 1 green-------------------------------")
+  temp = t1.green.signal
+  t1.green.signal[which(t1.green.model$classification == 1)] <- qnorm(pnorm(t1.green.signal[which(t1.green.model$classification == 1)],t1.green.mean[1], t1.green.sd[1]), ref$t1.green.ref.mean[1], ref$t1.green.ref.sd[1])
+  t1.green.signal[which(t1.green.model$classification == 2)] <- qnorm(pnorm(t1.green.signal[which(t1.green.model$classification == 2)],t1.green.mean[2], t1.green.sd[2]), ref$t1.green.ref.mean[2], ref$t1.green.ref.sd[2])
+
+  t1.green.signal[which(t1.green.signal == Inf)] = temp[which(t1.green.signal == Inf)]
+
+  m[t1.green.index] <- t1.green.signal[1:(length(t1.green.signal)/2)]
+  um[t1.green.index] <- t1.green.signal[(length(t1.green.signal)/2+1):length(t1.green.signal)]
+
+
+  print("Detecting p value-----------------------------------------------")
+  t2.index <- match(t2, probe)
+  t2.index <- t2.index[which(!is.na(t2.index))]
+  pII <- apply(cbind(1 - pnorm(um[t2.index], t1.red.mean[1], t1.red.sd[1]),
+                     1 - pnorm(m[t2.index], t1.green.mean[1], t1.green.sd[1])),
+               1, min)
+  p <- cbind(c(pIR, pIG , pII),
+             c(probe[t1.red.index], probe[t1.green.index], probe[t2.index]))
+  row.names(p) <- c(probe[t1.red.index], probe[t1.green.index], probe[t2.index])
+  m[which(m <= 0)] <- min(m[which(m > 0)])
+  um[which(um <= 0)] <- min(um[which(um > 0)])
+  normalized.signal <- data.frame(cbind(round(as.numeric(m), 0),
+                                        round(as.numeric(um), 0),
+                                        as.numeric(p[probe, 1])))
+  row.names(normalized.signal) <- probe
+  names(normalized.signal) <- c("m", "um", "p")
+
+  print("Normalizing between probe type----------------------------------")
+
+  swan.processed = modifiedSWAN(m, um, probe)
+
+  beta <- swan.processed$M / (swan.processed$M + swan.processed$U)
+  beta[which(normalized.signal$p >= 0.01)] = NA
+
+  return(beta)
+
+}
---
+++ R/modifiedSWAN.R
@@ -0,0 +1,120 @@
+modifiedSWAN <- function(Meth, Unmeth, probe) {
+
+  counts <- CpG.counts[CpG.counts$Name %in% probe, ]
+  subset <- min(
+    table(counts$nCpG[counts$Type == "I" & counts$nCpG %in% 1:3]),
+    table(counts$nCpG[counts$Type == "II" & counts$nCpG %in% 1:3]))
+
+  xNormSet <- lapply(c("I", "II"), function(type) {
+    getSubset(counts$nCpG[counts$Type == type], subset)
+  })
+
+  M <- SWAN(
+    x = Meth,
+    xNormSet = xNormSet,
+    counts = counts)
+  U <- SWAN(
+    x = Unmeth,
+    xNormSet = xNormSet,
+    counts = counts)
+
+  return(list(M = M, U = U))
+}
+
+getSubset <- function(counts, subset){
+  x <- integer(0)
+  for (i in 1:3) {
+    x <- c(x, sample(seq.int(1, length(counts))[counts == i], subset))
+  }
+  seq.int(1, length(counts)) %in% x
+}
+
+SWAN = function(x, xNormSet, counts) {
+  normalized_x <- matrix(NA_real_,
+                         ncol = ncol(x),
+                         nrow = nrow(x),
+                         dimnames = dimnames(x))
+  typeI_idx <- rownames(x) %in% counts$Name[counts$Type == "I"]
+  typeII_idx <- rownames(x) %in% counts$Name[counts$Type == "II"]
+  for (j in seq_len(ncol(x))) {
+    normalized_x[, j] <- normaliseChannel(
+      intensityI = x[typeI_idx, j],
+      intensityII = x[typeII_idx, j],
+      xNormSet = xNormSet)
+  }
+  normalized_x
+}
+
+normaliseChannel <- function(intensityI, intensityII, xNormSet) {
+  xTarget <- aveQuantile(
+    list(intensityI[xNormSet[[1]]], intensityII[xNormSet[[2]]]))
+  xNorm <- unlist(
+    subsetQuantileNorm(
+      list(intensityI, intensityII), xNormSet, xTarget))
+  names(xNorm) <- c(names(intensityI), names(intensityII))
+  xNorm
+}
+
+aveQuantile <- function(X) {
+  nbrOfChannels <- length(X)
+  if (nbrOfChannels == 1) {
+    return(X)
+  }
+  nbrOfObservations <- unlist(lapply(X, FUN = length), use.names = FALSE)
+  maxNbrOfObservations <- max(nbrOfObservations)
+  if (maxNbrOfObservations == 1) {
+    return(X)
+  }
+  ## nbrOfFiniteObservations <- rep(maxNbrOfObservations, times = nbrOfChannels)
+  quantiles <- (0:(maxNbrOfObservations - 1))/(maxNbrOfObservations - 1)
+  xTarget <- vector("double", maxNbrOfObservations)
+  for (cc in 1:nbrOfChannels) {
+    Xcc <- X[[cc]]
+    Scc <- sort(Xcc)
+    nobs <- length(Scc)
+    if (nobs < maxNbrOfObservations) {
+      ## tt <- !is.na(Xcc)
+      bins <- (0:(nobs - 1))/(nobs - 1)
+      Scc <- approx(x = bins, y = Scc, xout = quantiles,ties = "ordered")$y
+    }
+    xTarget <- xTarget + Scc
+  }
+  rm(Scc, Xcc)
+  xTarget <- xTarget/nbrOfChannels
+  xTarget
+}
+
+
+subsetQuantileNorm <- function(x, xNormSet, xTarget) {
+  for(i in 1:length(x)){
+    n <- length(x[[i]])
+    nTarget <- length(xTarget)
+    nNormSet <- sum(xNormSet[[i]])
+
+    if(nNormSet != nTarget){
+      targetQuantiles <- (0:(nTarget - 1))/(nTarget - 1)
+      r <- rank(x[xNormSet[,i], i])
+      xNew <-(r - 1)/(nNormSet - 1)
+      xNew <- xNew[order(xNew)]
+      xNorm <- approx(x = targetQuantiles, y = xTarget, xout = xNew, ties = "ordered", rule = 2)$y
+    } else {
+      xNorm<-xTarget
+    }
+
+    r <- rank(x[[i]])
+    xNew <-(r - 1)/(n - 1)
+    quantiles <- xNew[xNormSet[[i]]]
+    quantiles <- quantiles[order(quantiles)]
+    xmin <- min(x[[i]][xNormSet[[i]]]) #get min value from subset
+    xmax <- max(x[[i]][xNormSet[[i]]]) #get max value from subset
+    kmax <- which(xNew > max(quantiles))
+    kmin<- which(xNew < min(quantiles))
+    offsets.max <- x[[i]][kmax]-xmax
+    offsets.min <- x[[i]][kmin]-xmin
+    x[[i]] <- approx(x = quantiles, y = xNorm, xout = xNew, ties = "ordered")$y #interpolate
+    x[[i]][kmax]<- max(xNorm) + offsets.max
+    x[[i]][kmin]<- min(xNorm) + offsets.min
+    x[[i]] = ifelse(x[[i]] <= 0, min(x[[i]][which(x[[i]] > 0)]), x[[i]])
+  }
+  x
+}
